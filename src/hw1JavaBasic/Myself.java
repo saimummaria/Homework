d6c971c3d950485fa8545f66628e3b28b20050bd
saimummaria <7572maria@gmail.com>
2024-05-14 21:02:21 -0400

    Home work

---
+++ src/hw1JavaBasic/Myself.java
@@ -0,0 +1,19 @@
+package hw1JavaBasic;
+
+/*
+ * Project names generally starts with upperCase.
+ * Package names generally starts with lowerCase.
+ * Class names starts with upperCase.
+ * 
+ * Project,package,class follow Camel Case Or Snake case pattern.
+ */
+public class Myself {
+
+	public static void main(String[] args) {
+
+		System.out.println("Name:Maria Begum");
+		System.out.println("Address:2217 Falls st");
+		System.out.println("City:Niagarafalls NY");
+	}
+//For shortcut of main method Type 'main' inside the class body then press enter button.
+}
---
+++ src/hw3JavaVariablesDeclared/AboutMe.java
@@ -0,0 +1,26 @@
+package hw3JavaVariablesDeclared;
+
+public class AboutMe {
+	String myHobby;// variable declared
+	String myName = "Maria"; // variable initialized
+	int myAge = 21;
+	float myHeight = 5.6f;
+	char myGender = 'F';
+	boolean iAmBengali = true;
+
+	public AboutMe() { // constructor declared
+		System.out.println(" ------------ This is all about Myself -----------");
+	}
+
+	public void aboutMe() {
+		System.out.println("My Name: " + myName + "\nMy Age: " + myAge + "\nMy Gender" + myGender + "\nMy Height"
+				+ myHeight + "\nI am from Bangladesh: " + iAmBengali);
+	}
+
+	public static void main(String[] args) {
+		AboutMe aboutMe = new AboutMe();// constructor initialized
+		aboutMe.aboutMe();
+
+	}
+
+}
